ab8991e97f8bfc2428eab12df3796f04cff484d4
tmkim <xoals1417@gmail.com>
2024-11-07 16:51:51 +0900

    init project

---
+++ src/main/java/com/snowroad/SnowroadStarterApplication.java
@@ -0,0 +1,13 @@
+package com.snowroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowroadStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowroadStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowroad/SnowroadStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowroadStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
