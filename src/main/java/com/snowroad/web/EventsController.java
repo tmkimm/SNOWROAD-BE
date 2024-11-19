33c68ed2f772199f4fc3decaec4bb2ac793fee91
tmkimm <xoals1417@gmail.com>
2024-11-19 23:32:02 +0900

    feat: 삭제 api 명세 추가

--- src/main/java/com/snowroad/web/EventsController.java
+++ src/main/java/com/snowroad/web/EventsController.java
@@ -18,12 +18,12 @@ public class EventsController {
 
     @Operation(summary="팝업, 전시 등록", description = "(관리자) 이벤트를 등록합니다.")
     //@ApiResponse(responseCode = "200", description = "수신함 조회 성공", content = @Content(schema = @Schema(implementation = AlarmReceiveDto.class)))
-    @PostMapping("/api/v1/events")
+    @PostMapping("/api/events")
     public void save(@RequestBody EventsSaveRequestDto requestDto) {
 
     }
     @Operation(
-            summary = "이벤트 이미지 등록",
+            summary = "팝업, 전시 이미지 등록",
             description = "(관리자) 이벤트 이미지를 등록합니다.",
             requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
                     content = @Content(
@@ -36,7 +36,7 @@ public class EventsController {
                     @ApiResponse(responseCode = "400", description = "잘못된 요청")
             }
     )
-    @PostMapping("/{eventId}/files")
+    @PostMapping("events/{eventId}/files")
     public void uploadFiles(
             @PathVariable String eventId,
             @RequestParam("files") MultipartFile[] files,
@@ -48,5 +48,11 @@ public class EventsController {
         for (MultipartFile file : files) {
             System.out.println("업로드된 파일: " + file.getOriginalFilename());
         }
+    }
+
+    @Operation(summary="팝업, 전시 삭제", description = "(관리자) 이벤트를 삭제합니다.")
+    @DeleteMapping("/api/events/{eventId}")
+    public void delete(@PathVariable String eventId) {
+
     }
 }
