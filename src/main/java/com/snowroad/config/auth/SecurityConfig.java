9990b2f52bba7cb1abda14c9d8e8c29863392823
tmkimm <xoals1417@gmail.com>
2024-12-18 14:20:30 +0900

    feat: 구글 소셜 로그인 연동

--- src/main/java/com/snowroad/config/auth/SecurityConfig.java
+++ src/main/java/com/snowroad/config/auth/SecurityConfig.java
@@ -2,60 +2,50 @@ package com.snowroad.config.auth;
 
 
 import com.snowroad.domain.user.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+
+@RequiredArgsConstructor
 @EnableWebSecurity  // spring security 설정 활성화
+@Configuration
 public class SecurityConfig {
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        // // 정적 자원에 대해서는 Security 설정을 적용하지 않음.
-        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
+    private final CustomOAuth2UserService customOAuth2UserService;
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        // // 정적 자원에 대해서는 Security 설정을 적용하지 않음.
+//        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화 (선택 사항)
-                .headers((headersConfigurer -> {
+        http.csrf(csrf -> csrf.disable())  // h2-console을 위해 옵션을 disable
+                .headers((headersConfigurer) -> {
                     headersConfigurer.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable());
-                })) // h2-console 화면을 위해 해당 옵션 disable
+                })
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**")
-                        .permitAll()
-                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile")
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/swagger-ui/*")
                         .permitAll()// 모두 허용
-//                        .requestMatchers("api/**").hasRole(Role.USER.name())
-                        // /about 요청에 대해서는 로그인을 요구함
-                        .requestMatchers("/about").authenticated()
-                        // /admin 요청에 대해서는 ROLE_ADMIN 역할을 가지고 있어야 함
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        // 나머지 요청에 대해서는 로그인을 요구하지 않음
-                        .anyRequest().permitAll()
-                )
-                .logout(logout -> logout.logoutSuccessUrl("/"))
-//                .oauth2Login(oauth2 -> oauth2
-//                        .userInfoEndpoint(userInfo -> userInfo
-//                                .userService(customOAuth2UserService))
-//                )
-                .formLogin(formlogin -> formlogin
-                        .loginPage("/user/loginView")
-                        .successForwardUrl("/index")
-                        .failureForwardUrl("/index")
-                        .permitAll()
+//                        .requestMatchers("/api/**").hasRole(Role.USER.name())    // user 권한을 가진 사람만 허용
+                        .anyRequest().permitAll())  // 나머지 url은 모두 인증된 사용자만 허용
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/"))
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService))
                 );
-
-
         return http.build();
     }
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
---
+++ src/main/java/com/snowroad/web/IndexController.java
@@ -0,0 +1,24 @@
+package com.snowroad.web;
+
+import com.snowroad.config.auth.dto.SessionUser;
+import com.snowroad.web.dto.EventsSaveRequestDto;
+import jakarta.servlet.http.HttpSession;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+        private final HttpSession httpSession;
+        @GetMapping("/")
+        public String index(Model model) {
+                SessionUser user = (SessionUser) httpSession.getAttribute("user");
+                if(user != null) {
+                        model.addAttribute("userName", user.getName());
+                }
+                return "index";
+        }
+}
