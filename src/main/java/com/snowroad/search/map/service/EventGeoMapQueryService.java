6044b900eb3ae2ae090417ca074286cf2b0fdfd7
Hyo298 <zxcvb7587@naver.com>
2025-01-04 01:03:17 +0900

    feat(Map 커스텀 마커 조회): 조회 기능 구성 완료

--- src/main/java/com/snowroad/search/map/service/EventGeoMapQueryService.java
+++ src/main/java/com/snowroad/search/map/service/EventGeoMapQueryService.java
@@ -7,11 +7,13 @@ import com.snowroad.search.map.interfaces.EventGeoMapQueryInterface;
 import com.snowroad.search.map.util.BoundingBoxCalculator;
 import com.snowroad.search.map.util.Haversine;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class EventGeoMapQueryService implements EventGeoMapQueryInterface {
@@ -29,16 +31,19 @@ public class EventGeoMapQueryService implements EventGeoMapQueryInterface {
         double[] calculateBoundingBox = BoundingBoxCalculator.calculateBoundingBox(latitude, longitude);
         List<EventsGeoMapDto> candidates = eventsGeoRepository
                 .findEventsGeoCustomPoint(calculateBoundingBox[0], calculateBoundingBox[1], calculateBoundingBox[2], calculateBoundingBox[3]);
+
         List<EventsGeoMapDto> result = new LinkedList<>();
         for (EventsGeoMapDto eventsGeoMapDto : candidates) {
             double distance = Haversine.calculateDistance(
                     latitude, longitude, eventsGeoMapDto.getAddrLttd(), eventsGeoMapDto.getAddrLotd()
             );
+
             if (distance <= GeoMapEnum.MAP_DISTANCE_STANDARD.getRate()) {
+                log.info("거리 계산 :: {}",distance);
+                log.info("이벤트명 :: {}",eventsGeoMapDto.getEvntNm());
                 result.add(eventsGeoMapDto);
             }
         }
-
         return result;
     }
 }
--- src/main/java/com/snowroad/search/map/web/EventGeoMapQueryController.java
+++ src/main/java/com/snowroad/search/map/web/EventGeoMapQueryController.java
@@ -8,12 +8,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @Tag(name = "Map 커스텀 마커 조회", description = "Map 커스텀 마커 구성시 사용하는 API")
@@ -24,8 +26,10 @@ public class EventGeoMapQueryController {
     @Operation(summary="이벤트 커스텀 마커 조회", description = "등록된 이벤트를 조회합니다")
     @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = EventsGeoMapDto.class)))
     @GetMapping("/api/search/events")
-    List<EventsGeoMapDto> getList(@RequestParam(defaultValue = "37.5540219315164") double addrLttd
+    List<EventsGeoMapDto> getEventsGeoMapList(@RequestParam(defaultValue = "37.5540219315164") double addrLttd
             , @RequestParam(defaultValue = "126.922884921727") double addrLotd) {
-       return eventGeoMapQueryInterface.getMapList(addrLttd, addrLotd);
+        List<EventsGeoMapDto> list = eventGeoMapQueryInterface.getMapList(addrLttd, addrLotd);
+        log.info("getEventsGeoMapList {} ",list);
+       return list;
     }
 }
