c01699dcc0dfd5d95c63aa8d03e316d7f389521a
tmkim <xoals1417@gmail.com>
2025-03-05 13:14:03 +0900

    feat: 카테고리 추가, 수정 시 인가 체크

--- src/main/java/com/snowroad/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/snowroad/common/exception/GlobalExceptionHandler.java
@@ -13,5 +13,14 @@ public class GlobalExceptionHandler {
         // 400 Bad Request 응답과 함께 메시지 반환
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<String> handleForbiddenException(ForbiddenException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleForbiddenException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
 
 }
