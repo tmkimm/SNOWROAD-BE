6a2f64c5de9b1aa914a6f76554220f51bcf079cf
tmkimm <xoals1417@gmail.com>
2025-03-17 21:33:51 +0900

    cors: 프론트 서버 cors 허용

--- src/main/java/com/snowroad/config/WebConfig.java
+++ src/main/java/com/snowroad/config/WebConfig.java
@@ -40,7 +40,7 @@ public class WebConfig  implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 요청에 대해 CORS 허용
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3000/", "http://127.0.0.1:3000") // 모든 출처 허용
+                .allowedOrigins("http://localhost:3000", "http://localhost:3000/", "http://127.0.0.1:3000", "http://ec2-13-125-216-97.ap-northeast-2.compute.amazonaws.com") // 모든 출처 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 자격 증명(쿠키 등) 허용
