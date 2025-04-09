e49820fa22b09de2210c9b771dcbb25f7f56399a
tmkim <xoals1417@gmail.com>
2025-04-09 13:08:10 +0900

    fix: komoran.dictionary-path 공백 제거(travis 에러 해결을 위해)

--- src/test/java/com/snowroad/SnowroadStarterApplicationTests.java
+++ src/test/java/com/snowroad/SnowroadStarterApplicationTests.java
@@ -3,7 +3,7 @@ package com.snowroad;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SnowroadStarterApplication.class)
 class SnowroadStarterApplicationTests {
 
 	@Test
