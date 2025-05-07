2d280e513de1ca826a8a9e52ea1e328ae8467398
hrpark <phr038942@gmail.com>
2025-05-07 20:46:51 +0900

    fix(events-sort) : 지역순 정렬조건 추가

--- src/main/java/com/snowroad/event/impl/EventsRepositoryImpl.java
+++ src/main/java/com/snowroad/event/impl/EventsRepositoryImpl.java
@@ -116,20 +116,10 @@ public class EventsRepositoryImpl implements EventsRepositoryCustom {
         LocalDate today = LocalDate.now();
         String todayStr = today.format(DateTimeFormatter.ofPattern("yyyyMMdd")); // 현재일자를 yyyyMMdd 포맷으로 변환
 
-//        List<String> categoryKeys = ctgyId.stream()
-//                .map(Category::getKey) // Enum에서 key 값만 추출
-//                .toList();
-
-
         // WHERE 조건 동적 추가
         BooleanBuilder whereCondition = new BooleanBuilder();
         whereCondition.and(e.deleteYn.eq("N")); // 기본 조건 유지 (삭제여부)
 
-        // userId값이 있는 경우 좋아요여부 체킹
-//        BooleanExpression markJoinCondition = null;
-//        if (userId != null && userId != 0) {
-//            userId = 0L;
-//        }
         // categoryList 값이 존재할 때만 IN 조건 추가
         if (ctgyId != null && !ctgyId.isEmpty()) {
             whereCondition.and(e.ctgyId.in(ctgyId));
@@ -143,11 +133,13 @@ public class EventsRepositoryImpl implements EventsRepositoryCustom {
         if (geo != null && !geo.isEmpty()) {
             whereCondition.and(region.rgntCd.in(geo));
         }
-        // sortType 10:조회순 20:최신순(오픈일순) 30:마감순(마감임박순) 40:지역별(지역명칭)-미완 50:거리순-미완
+        // sortType 10:조회순 20:최신순(오픈일순) 30:마감순(마감임박순) 40:지역별(지역명칭)-미완
         // sortType = 30일 때는 `operEndDt`가 현재일자 이후여야 함
         if ("30".equals(sortType)) {
             whereCondition.and(e.operEndDt.goe(todayStr)); // 현재일자보다 이후 데이터
         }
+
+
         // ORDER BY 조건 설정,
         OrderSpecifier<?> orderBy = e.operStatDt.asc(); // 기본 정렬
         if ("10".equals(sortType)) {
@@ -157,9 +149,7 @@ public class EventsRepositoryImpl implements EventsRepositoryCustom {
         } else if ("30".equals(sortType)) {
             orderBy = e.operEndDt.asc();
         } else if ("40".equals(sortType)) {
-            //    orderBy = e.operEndDt.asc();
-        } else if ("50".equals(sortType)) {
-            //    orderBy = e.operEndDt.asc();
+            orderBy = e.lnad.asc();
         }
 
         // 전체 데이터 개수 조회 (페이징을 위한 count 쿼리)
@@ -218,79 +208,6 @@ public class EventsRepositoryImpl implements EventsRepositoryCustom {
         return new PageImpl<>(result, page, total); // 페이징된 결과 반환
     }
 
-//    public EventContentsResponseDto findEvntData(Long evntId, Long userId){
-//
-//        QEvents e = QEvents.events;
-//        QMark mark = QMark.mark;
-//        QView view = QView.view;
-//        QRegion region = QRegion.region;
-//        QEventFilesMst fileMst = QEventFilesMst.eventFilesMst;
-//        QEventFilesDtl fileDtl = QEventFilesDtl.eventFilesDtl;
-//
-//
-//        LocalDate today = LocalDate.now();
-//        String todayStr = today.format(DateTimeFormatter.ofPattern("yyyyMMdd")); // 현재일자를 yyyyMMdd 포맷으로 변환
-//
-//        BooleanExpression likeCondition;
-//        if (userId != null) {
-//            likeCondition = mark.userAcntNo.eq(userId);
-//        }
-//        else likeCondition = Expressions.FALSE;
-//
-//        Tuple tupleResult = queryFactory
-//                .select(
-//                        e.eventId,
-//                        e.eventNm,
-//                        e.eventCntn,
-//                        e.eventAddr,
-//                        e.rads,
-//                        e.lnad,
-//                        e.operStatDt,
-//                        e.operEndDt,
-//                        e.operDttmCntn,
-//                        e.ctgyId,
-//                        e.eventTypeCd,
-//                        Expressions.cases()
-//                                .when(mark.likeYn.isNotNull()).then(mark.likeYn.stringValue())
-//                                .otherwise("N").as("likeYn"),
-//                        view.viewNwvl.coalesce(0),
-//                        fileDtl.fileUrl,
-//                        fileDtl.fileThumbUrl
-//                )
-//                .from(e)
-//                .leftJoin(view).on(e.eventId.eq(view.eventId)) // JOIN TB_EVNT_VIEW_D
-//                .leftJoin(mark).on(e.eventId.eq(mark.eventId).and(likeCondition))
-//                .leftJoin(fileMst).on(e.eventTumbfile.fileMstId.eq(fileMst.fileMstId)) // JOIN TB_EVNT_FILE_M
-//                .leftJoin(fileDtl).on(fileMst.fileMstId.eq(fileDtl.fileMst.fileMstId)) // JOIN TB_EVNT_FILE_D
-//                .where(
-//                        e.eventId.eq(evntId)
-//                )
-//                .fetchOne();
-//        System.out.println(tupleResult.get(view.viewNwvl));
-//        System.out.println(tupleResult.get(view.eventId));
-////        String likeYn = tupleResult.get(mark.likeYn.coalesce("N"));
-//
-//        EventContentsResponseDto dto = (tupleResult != null) ? new EventContentsResponseDto(
-//                tupleResult.get(e.eventId),
-//                tupleResult.get(e.eventNm),
-//                tupleResult.get(e.eventCntn),
-//                tupleResult.get(e.eventAddr),
-//                tupleResult.get(e.rads),
-//                tupleResult.get(e.lnad),
-//                tupleResult.get(e.operStatDt),
-//                tupleResult.get(e.operEndDt),
-//                tupleResult.get(e.operDttmCntn),
-//                tupleResult.get(e.ctgyId),
-//                tupleResult.get(e.eventTypeCd),
-//                tupleResult.get(Expressions.stringPath("likeYn")),
-//                (tupleResult.get(view.viewNwvl) != null) ? tupleResult.get(view.viewNwvl) : 0, // Null 체크 추가
-//                tupleResult.get(fileDtl.fileUrl),
-//                tupleResult.get(fileDtl.fileThumbUrl)
-//        ) : null;
-//
-//
-//        return dto;
-//    }
 
 public EventContentsResponseDto findEvntData(Long evntId, Long userId){
 
