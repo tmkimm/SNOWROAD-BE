eca4de1ae92811637fee088582dee00b2b3d8591
tmkimm <xoals1417@gmail.com>
2025-07-16 22:16:13 +0900

    test: 어드민 - 이벤트 삭제 테스트 케이스 추가

--- src/test/java/com/snowroad/event/web/AdminControllerTest.java
+++ src/test/java/com/snowroad/event/web/AdminControllerTest.java
@@ -18,12 +18,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -151,5 +151,59 @@ class AdminControllerTest {
         Events events = all.get(0);
         assertThat(events.getEventNm()).isEqualTo(expectedEventNm);
         assertThat(events.getEventCntn()).isEqualTo(expectedEventCntn);
+    }
+
+    @Test
+    @WithMockUser(roles="USER")
+    void Events_삭제된다() throws Exception{
+        //given
+        String eventNm = "Summer Sale";
+        String eventCntn = "Discounts on all summer items.";
+        String eventAddr = "123 Summer St, Beach City";
+        String operStatDt = "20240101";
+        String operEndDt = "20240131";
+        String operDttmCntn = "Mon-Fri, 9 AM to 6 PM";
+        String ctgyId = "SUMMER2024";
+        String eventTypeCd = "POPUP";
+        Double addrLttd = 34.0522;  // 위도 (예: LA)
+        Double addrLotd = -118.2437;  // 경도 (예: LA)
+        Events savedEvents = eventsRepository.save(Events.builder()
+                .eventNm(eventNm)
+                .eventCntn(eventCntn)
+                .eventAddr(eventAddr)
+                .operStatDt(operStatDt)
+                .operEndDt(operEndDt)
+                .operDttmCntn(operDttmCntn)
+                .ctgyId(ctgyId)
+                .eventTypeCd(eventTypeCd)
+                .addrLttd(addrLttd)
+                .addrLotd(addrLotd)
+                .build());
+        Long updateId = savedEvents.getEventId();
+        String expectedEventNm = "123 Summer Sale";
+        String expectedEventCntn = "456 Discounts on all summer items.";
+
+        EventsSaveRequestDto requestDto = EventsSaveRequestDto.builder()
+                .eventNm(expectedEventNm)
+                .eventCntn(expectedEventCntn)
+                .eventAddr(eventAddr)
+                .operStatDt(operStatDt)
+                .operEndDt(operEndDt)
+                .operDttmCntn(operDttmCntn)
+                .ctgyId(ctgyId)
+                .ppstEnbnTypeCd(eventTypeCd)
+                .addrLttd(addrLttd)
+                .addrLotd(addrLotd)
+                .build();
+        String url = "http://localhost:"+port+"/api/admin/events/"+updateId;
+
+        HttpEntity<EventsSaveRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        mvc.perform(delete(url)).andExpect(status().isOk());
+
+        // then
+        Optional<Events> deletedEvent = eventsRepository.findById(updateId);
+        assertThat(deletedEvent.isPresent()).isFalse();
     }
 }
